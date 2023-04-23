d64d3b87b299285a10071f6749b35f621058656f
Gilvan Zacarias <gilvan.zacarias@outlook.com>
2023-04-23 10:13:20 -0300

    feat: add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import Header from './components/header';
+import ButtonAppBar from './components/ButtonAppBar';
 import Body from './components/body';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Header/>
-      </header>
+      <ButtonAppBar/>
       
       <Body/>
     </div>
--- src/components/body/index.jsx
+++ src/components/body/index.jsx
@@ -1,14 +1,7 @@
 import React from 'react'
-import LearnReact from '../link'
-import Description from '../description'
-import Logo from '../logo'
 
 export default function Body() {
   return (
-    <div>
-        <Logo/>
-        <Description/>
-        <LearnReact/>
-    </div>
+    <div>BODY</div>
   )
 }
